d1bbe1c5960e0ea319ffb0775a399f7f05d170ee
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-04-08 19:03:54 -0500

    Add the DOM

---
+++ main.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("#newGame");
+const diceValue1 = document.querySelector("#d1");
+const diceValue2 = document.querySelector("#d2");
+const diceValue3 = document.querySelector("#d3");
+const diceValue4 = document.querySelector("#d4");
+const diceValue5 = document.querySelector("#d5");
+const diceValue6 = document.querySelector("#b1");
+const diceValue7 = document.querySelector("#b2");
+
+
+let roll = () => {
+    return Math.floor(Math.random()*6)+1;
+};
+
+let rollAllDice = () => {
+  diceValue1.innerHTML = roll();
+  diceValue2.innerHTML = roll();
+  diceValue3.innerHTML = roll();
+  diceValue4.innerHTML = roll();
+  diceValue5.innerHTML = roll();
+  diceValue6.innerHTML = roll();
+  diceValue7.innerHTML = roll()*10;
+}
+
+button.onclick = rollAllDice;
