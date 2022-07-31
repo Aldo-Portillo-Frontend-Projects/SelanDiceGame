cf980d658e0780ae940f650ccc4e6dfad509516c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-07-31 18:02:08 -0500

    Clean up comments

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-
-/*const diceValue1 = document.querySelector("#d1");
-const diceValue2 = document.querySelector("#d2");
-const diceValue3 = document.querySelector("#d3");
-const diceValue4 = document.querySelector("#d4");
-const diceValue5 = document.querySelector("#d5");
-const diceValue6 = document.querySelector("#b1");
-const diceValue7 = document.querySelector("#b2");
-
-
-
-
-let rollAllDice = () => {
-  diceValue1.innerHTML = roll();
-  diceValue2.innerHTML = roll();
-  diceValue3.innerHTML = roll();
-  diceValue4.innerHTML = roll();
-  diceValue5.innerHTML = roll();
-  diceValue6.innerHTML = roll();
-  diceValue7.innerHTML = roll()*10;
-}
-
-button.onclick = rollAllDice;
-
-*/
-
 const button = document.querySelector("button");
 const die = document.querySelectorAll(".dice");
 
